fe457fe33c1ed0376210b0e8b97b690982986d1a
Krzysztof <kpiechowski25@gmail.com>
2023-06-08 20:12:21 +0200

    your message

---
+++ src/backend/routes/tasks/delete.task.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { body } from 'express-validator'
+import { TRoute } from '../types'
+import { handleRequest } from '../../utils/request.utils'
+import { authorize } from '../../utils/middleware.utils'
+import { markTask } from '../../services/task.service'
+import { RequestForUser } from '../../services/task.service'
+export default {
+    method: 'get',
+    path: '/api/task/mark',
+    validators: [authorize, body('taskId').not().isEmpty()],
+
+    handler: async (req: RequestForUser, res: Response) =>
+        handleRequest({
+            req,
+            res,
+            responseSuccessStatus: StatusCodes.OK,
+            execute: async () => {
+                const userId = req.user.id
+                await markTask(req.body(), userId)
+            },
+        }),
+} as TRoute
---
+++ src/backend/routes/tasks/mark.task.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { body } from 'express-validator'
+import { TRoute } from '../types'
+import { handleRequest } from '../../utils/request.utils'
+import { authorize } from '../../utils/middleware.utils'
+import { addTask } from '../../services/task.service'
+
+export default {
+    method: 'post',
+    path: '/api/task/add',
+    validators: [
+        authorize,
+        body('title').not().isEmpty(),
+        body('complete').isBoolean(),
+        body('description').not().isEmpty(),
+        body('deadLineDate').not().isEmpty(),
+        body('taskStatus').not().isEmpty(),
+    ],
+    handler: async (req: Request, res: Response) =>
+        handleRequest({
+            req,
+            res,
+            responseSuccessStatus: StatusCodes.CREATED,
+            execute: async () => await addTask(req.body),
+        }),
+} as TRoute
